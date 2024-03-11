8293cc7470fdffc9af48f498d50f0049832bbfbc
aaa <aaa@bbbcom>
2024-03-11 10:05:43 +0800

    update: 自定义翻译入口

--- Sources/FaceLiveness/Utilities/String+Localizable.swift
+++ Sources/FaceLiveness/Utilities/String+Localizable.swift
@@ -7,11 +7,26 @@
 
 import Foundation
 
+public protocol CustomeLivenessStringProtocol {
+    func localized(key: String, comment: String) -> String?
+}
+
+public class CustomeLivenessString {
+    public static var customLocalDelegate: CustomeLivenessStringProtocol? = nil
+}
+
 extension String {
     /// Looks for a localized value using this value as the key.
     /// If no localization is found in the current app's bundle,
     /// it defaults to the one provided by Liveness
     func localized(comment: String = "") -> String {
+        
+        if let delegate = CustomeLivenessString.customLocalDelegate,
+           let val = delegate.localized(key: self, comment: "") {
+            // 存在外测代理，使用外测的返回结果
+           return val
+        }
+        
         let defaultValue = NSLocalizedString(self, bundle: .module, comment: "")
         return NSLocalizedString(
             self,
