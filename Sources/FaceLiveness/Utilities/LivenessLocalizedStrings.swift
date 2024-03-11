5e77c2b1b921272391e838ef4402416b9c5258f7
aaa <aaa@bbbcom>
2024-03-11 15:20:05 +0800

    update: 解决由于使用static let导致有缓存，App内切换语言会出现语言没有切换的问题

--- Sources/FaceLiveness/Utilities/LivenessLocalizedStrings.swift
+++ Sources/FaceLiveness/Utilities/LivenessLocalizedStrings.swift
@@ -9,113 +9,187 @@ import SwiftUI
 
 enum LocalizedStrings {
     /// en = "Liveness Check"
-    static let get_ready_page_title = "amplify_ui_liveness_get_ready_page_title".localized()
+    static var get_ready_page_title: String {
+        return "amplify_ui_liveness_get_ready_page_title".localized()
+    }
     
     /// en = "Photosensitivity Warning"
-    static let get_ready_photosensitivity_title = "amplify_ui_liveness_get_ready_photosensitivity_title".localized()
+    static var get_ready_photosensitivity_title: String {
+        return "amplify_ui_liveness_get_ready_photosensitivity_title".localized()
+    }
 
     /// en = "This check displays colored lights. Use caution if you are photosensitive."
-    static let get_ready_photosensitivity_description = "amplify_ui_liveness_get_ready_photosensitivity_description".localized()
+    static var get_ready_photosensitivity_description: String {
+        return "amplify_ui_liveness_get_ready_photosensitivity_description".localized()
+    }
 
     /// en = "Photosensitivity Information"
-    static let get_ready_photosensitivity_icon_a11y = "amplify_ui_liveness_get_ready_photosensitivity_icon_a11y".localized()
+    static var get_ready_photosensitivity_icon_a11y: String {
+        return "amplify_ui_liveness_get_ready_photosensitivity_icon_a11y".localized()
+    }
 
     /// en = "Photosensitivity warning"
-    static let get_ready_photosensitivity_dialog_title = "amplify_ui_liveness_get_ready_photosensitivity_dialog_title".localized()
+    static var get_ready_photosensitivity_dialog_title: String {
+        return "amplify_ui_liveness_get_ready_photosensitivity_dialog_title".localized()
+    }
 
     /// en = "A small percentage of individuals may experience epileptic seizures when exposed to colored lights. Use caution if you, or anyone in your family, have an epileptic condition."
-    static let get_ready_photosensitivity_dialog_description = "amplify_ui_liveness_get_ready_photosensitivity_dialog_description".localized()
+    static var get_ready_photosensitivity_dialog_description: String {
+        return "amplify_ui_liveness_get_ready_photosensitivity_dialog_description".localized()
+    }
 
     /// en = "Start video check"
-    static let get_ready_begin_check = "amplify_ui_liveness_get_ready_begin_check".localized()
+    static var get_ready_begin_check: String {
+        return "amplify_ui_liveness_get_ready_begin_check".localized()
+    }
 
     /// en = "REC"
-    static let challenge_recording_indicator_label = "amplify_ui_liveness_challenge_recording_indicator_label".localized()
+    static var challenge_recording_indicator_label: String {
+        return "amplify_ui_liveness_challenge_recording_indicator_label".localized()
+    }
 
     /// en = "Hold face in oval for colored lights."
-    static let challenge_instruction_hold_face_during_freshness = "amplify_ui_liveness_challenge_instruction_hold_face_during_freshness".localized()
+    static var challenge_instruction_hold_face_during_freshness: String {
+        return "amplify_ui_liveness_challenge_instruction_hold_face_during_freshness".localized()
+    }
 
     /// en = "Move back"
-    static let challenge_instruction_move_face_back = "amplify_ui_liveness_challenge_instruction_move_face_back".localized()
+    static var challenge_instruction_move_face_back: String {
+        return "amplify_ui_liveness_challenge_instruction_move_face_back".localized()
+    }
 
     /// en = "Move closer"
-    static let challenge_instruction_move_face_closer = "amplify_ui_liveness_challenge_instruction_move_face_closer".localized()
+    static var challenge_instruction_move_face_closer: String {
+        return "amplify_ui_liveness_challenge_instruction_move_face_closer".localized()
+    }
 
     /// en = "Move closer"
-    static let challenge_instruction_move_face = "amplify_ui_liveness_challenge_instruction_move_face".localized()
+    static var challenge_instruction_move_face: String {
+        return "amplify_ui_liveness_challenge_instruction_move_face".localized()
+    }
 
     /// en = "Hold still"
-    static let challenge_instruction_hold_still = "amplify_ui_liveness_challenge_instruction_hold_still".localized()
+    static var challenge_instruction_hold_still: String {
+        return "amplify_ui_liveness_challenge_instruction_hold_still".localized()
+    }
 
     /// en = "Only one face per check"
-    static let challenge_instruction_multiple_faces_detected = "amplify_ui_liveness_challenge_instruction_multiple_faces_detected".localized()
+    static var challenge_instruction_multiple_faces_detected: String {
+        return "amplify_ui_liveness_challenge_instruction_multiple_faces_detected".localized()
+    }
 
     /// en = "Connecting..."
-    static let challenge_connecting = "amplify_ui_liveness_challenge_connecting".localized()
+    static var challenge_connecting: String {
+        return "amplify_ui_liveness_challenge_connecting".localized()
+    }
 
     /// en = "Verifying"
-    static let challenge_verifying = "amplify_ui_liveness_challenge_verifying".localized()
+    static var challenge_verifying: String {
+        return "amplify_ui_liveness_challenge_verifying".localized()
+    }
 
     /// en = "Cancel Challenge"
-    static let challenge_cancel_a11y = "amplify_ui_liveness_challenge_cancel_a11y".localized()
+    static var challenge_cancel_a11y: String {
+        return "amplify_ui_liveness_challenge_cancel_a11y".localized()
+    }
 
     /// en = "Change Your Camera Settings"
-    static let camera_setting_alert_title = "amplify_ui_liveness_camera_setting_alert_title".localized()
+    static var camera_setting_alert_title: String {
+        return "amplify_ui_liveness_camera_setting_alert_title".localized()
+    }
 
     /// en = "Allow camera permission in settings."
-    static let camera_setting_alert_message = "amplify_ui_liveness_camera_setting_alert_message".localized()
+    static var camera_setting_alert_message: String {
+        return "amplify_ui_liveness_camera_setting_alert_message".localized()
+    }
 
     /// en = "Update Setting"
-    static let camera_setting_alert_update_setting_button_text = "amplify_ui_liveness_camera_setting_alert_update_setting_button_text".localized()
+    static var camera_setting_alert_update_setting_button_text: String {
+        return "amplify_ui_liveness_camera_setting_alert_update_setting_button_text".localized()
+    }
 
     /// en = "Not Now"
-    static let camera_setting_alert_not_now_button_text = "amplify_ui_liveness_camera_setting_alert_not_now_button_text".localized()
+    static var camera_setting_alert_not_now_button_text: String {
+        return "amplify_ui_liveness_camera_setting_alert_not_now_button_text".localized()
+    }
 
     /// en = "Close"
-    static let close_button_a11y = "amplify_ui_liveness_close_button_a11y".localized()
+    static var close_button_a11y: String {
+        return "amplify_ui_liveness_close_button_a11y".localized()
+    }
     
     /// en = "Center your face"
-    static let preview_center_your_face_text = "amplify_ui_liveness_center_your_face_text".localized()
+    static var preview_center_your_face_text: String {
+        return "amplify_ui_liveness_center_your_face_text".localized()
+    }
     
     /// en = "Liveness check"
-    static let camera_permission_page_title = "amplify_ui_liveness_camera_permission_page_title".localized()
+    static var camera_permission_page_title: String {
+        return "amplify_ui_liveness_camera_permission_page_title".localized()
+    }
     
     /// en = "Change Camera Setting"
-    static let camera_permission_change_setting_button_title = "amplify_ui_liveness_camera_permission_button_title".localized()
+    static var camera_permission_change_setting_button_title: String {
+        return "amplify_ui_liveness_camera_permission_button_title".localized()
+    }
     
     /// en = "Camera is not accessible"
-    static let camera_permission_change_setting_header = "amplify_ui_liveness_camera_permission_button_header".localized()
+    static var camera_permission_change_setting_header: String {
+        return "amplify_ui_liveness_camera_permission_button_header".localized()
+    }
     
     /// en = "You may have to go into settings to grant camera permissions and close the app and retry"
-    static let camera_permission_change_setting_description = "amplify_ui_liveness_camera_permission_button_description".localized()
+    static var camera_permission_change_setting_description: String {
+        return "amplify_ui_liveness_camera_permission_button_description".localized()
+    }
     
     /// en = ""
-    static let amplify_ui_liveness_face_not_prepared_reason_pendingCheck = "amplify_ui_liveness_face_not_prepared_reason_pendingCheck".localized()
+    static var amplify_ui_liveness_face_not_prepared_reason_pendingCheck: String {
+        return "amplify_ui_liveness_face_not_prepared_reason_pendingCheck".localized()
+    }
     
     /// en = "Move face to fit in oval"
-    static let amplify_ui_liveness_face_not_prepared_reason_not_in_oval = "amplify_ui_liveness_face_not_prepared_reason_not_in_oval".localized()
+    static var amplify_ui_liveness_face_not_prepared_reason_not_in_oval: String {
+        return "amplify_ui_liveness_face_not_prepared_reason_not_in_oval".localized()
+    }
     
     /// en = "Move closer"
-    static let amplify_ui_liveness_face_not_prepared_reason_move_face_closer = "amplify_ui_liveness_face_not_prepared_reason_move_face_closer".localized()
+    static var amplify_ui_liveness_face_not_prepared_reason_move_face_closer: String {
+        return "amplify_ui_liveness_face_not_prepared_reason_move_face_closer".localized()
+    }
     
     /// en = "Move face right"
-    static let amplify_ui_liveness_face_not_prepared_reason_move_face_right = "amplify_ui_liveness_face_not_prepared_reason_move_face_right".localized()
+    static var amplify_ui_liveness_face_not_prepared_reason_move_face_right: String {
+        return "amplify_ui_liveness_face_not_prepared_reason_move_face_right".localized()
+    }
     
     /// en = "Move face left"
-    static let amplify_ui_liveness_face_not_prepared_reason_move_face_left = "amplify_ui_liveness_face_not_prepared_reason_move_face_left".localized()
+    static var amplify_ui_liveness_face_not_prepared_reason_move_face_left: String {
+        return "amplify_ui_liveness_face_not_prepared_reason_move_face_left".localized()
+    }
     
     /// en = "Move to dimmer area"
-    static let amplify_ui_liveness_face_not_prepared_reason_move_to_dimmer_area = "amplify_ui_liveness_face_not_prepared_reason_move_to_dimmer_area".localized()
+    static var amplify_ui_liveness_face_not_prepared_reason_move_to_dimmer_area: String {
+        return "amplify_ui_liveness_face_not_prepared_reason_move_to_dimmer_area".localized()
+    }
     
     /// en = "Move to brighter area"
-    static let amplify_ui_liveness_face_not_prepared_reason_move_to_brighter_area = "amplify_ui_liveness_face_not_prepared_reason_move_to_brighter_area".localized()
+    static var amplify_ui_liveness_face_not_prepared_reason_move_to_brighter_area: String {
+        return "amplify_ui_liveness_face_not_prepared_reason_move_to_brighter_area".localized()
+    }
     
     /// en = "Move face in front of camera"
-    static let amplify_ui_liveness_face_not_prepared_reason_no_face = "amplify_ui_liveness_face_not_prepared_reason_no_face".localized()
+    static var amplify_ui_liveness_face_not_prepared_reason_no_face: String {
+        return "amplify_ui_liveness_face_not_prepared_reason_no_face".localized()
+    }
     
     /// en = "Ensure only one face is in front of camera"
-    static let amplify_ui_liveness_face_not_prepared_reason_multiple_faces = "amplify_ui_liveness_face_not_prepared_reason_multiple_faces".localized()
+    static var amplify_ui_liveness_face_not_prepared_reason_multiple_faces: String {
+        return "amplify_ui_liveness_face_not_prepared_reason_multiple_faces".localized()
+    }
     
     /// en = "Move face farther away"
-    static let amplify_ui_liveness_face_not_prepared_reason_face_too_close = "amplify_ui_liveness_face_not_prepared_reason_face_too_close".localized()
+    static var amplify_ui_liveness_face_not_prepared_reason_face_too_close: String {
+        return "amplify_ui_liveness_face_not_prepared_reason_face_too_close".localized()
+    }
 }
